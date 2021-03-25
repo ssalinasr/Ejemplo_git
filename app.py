26b7053dd29d56cb15ec72252976bfc51181b528
ssalinasr <ssalinasr@correo.udistrital.edu.co>
2021-03-25 11:05:19 -0500

    mas cambios

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(_name_)
 def hola():
     return "Hola mundo"
 
+@app.route("/suma/<val_1>/<val_2>")
+def suma(val_1, val_2):
+    c = Calculadora()
+    c.val1 = val_1
+    c.val2 = val_2
+    c.suma()
+
 if _name_ == '__main__':
     app.run(debug= True)
+

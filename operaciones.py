789c288de29c977aafc98ddf8a41acca6bbb9584
ssalinasr <ssalinasr@correo.udistrital.edu.co>
2021-03-25 10:48:39 -0500

    Primera version

---
+++ operaciones.py
@@ -0,0 +1,22 @@
+class operaciones:
+
+    def _init_(self):
+        self.val1 = 0
+        self.val2 = 0
+        self.res = 0
+
+    def suma(self):
+        self.res = self.val1 + self.val2
+        print('El resultado es: '+str(self.res))
+    
+    def resta(self):
+        self.res = self.val1 - self.val2
+        print('El resultado es: '+str(self.res))
+
+    def multiplicacion(self):
+        self.res = self.val1 * self.val2
+        print('El resultado es: '+str(self.res))
+
+    
+
+        
---
+++ test.py
@@ -0,0 +1,10 @@
+from operaciones import operaciones
+
+c = operaciones()
+
+c.val1 = 6
+c.val2 = 8
+
+c.suma()
+c.resta()
+c.multiplicacion()

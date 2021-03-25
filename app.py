123333e6adec4c0e67aba32085abf8be123008f3
ssalinasr <ssalinasr@correo.udistrital.edu.co>
2021-03-25 11:13:32 -0500

    cambios lol

--- app.py
+++ app.py
@@ -13,6 +13,20 @@ def suma(val_1, val_2):
     c.val2 = val_2
     c.suma()
 
+@app.route("/resta/<int:val_1>/<int:val_2>")
+def resta(val_1, val_2):
+    c = Calculadora()
+    c.val1 = val_1
+    c.val2 = val_2
+    c.suma()
+
+@app.route("/multiplicacion/<int:val_1>/<int:val_2>")
+def multiplicacion(val_1, val_2):
+    c = Calculadora()
+    c.val1 = val_1
+    c.val2 = val_2
+    c.suma()
+
 if _name_ == '__main__':
     app.run(debug= True)
 
